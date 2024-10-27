1fc453cc178e5c13d290cbdd1df3db9d05842ee3
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2024-10-27 18:59:13 -0300

    refactor: data

--- src/controllers/registrosController.js
+++ src/controllers/registrosController.js
@@ -1,18 +1,15 @@
-import db from "../index.js"
+import db from "../index.js";
 
-export async function getRegistros(req, res){
-    try {
-        const registros = await db.collection("registros").find().toArray();
-        if (!registros) {
-          return res.sendStatus(404);
-        }
-    
-        res.send(registros);
-      } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-      }
+export async function getRegistros(req, res) {
+  try {
+    const registros = await db.collection("registros").find().toArray();
+    if (!registros) {
+      return res.sendStatus(404);
+    }
 
-        
-      
+    res.send(registros);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
